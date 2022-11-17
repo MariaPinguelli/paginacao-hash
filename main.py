bbc9d4949d09f8094f32e098434632307d5926de
MariaPinguelli <mariaferpinguelli@hotmail.com>
2022-11-17 17:44:26 -0300

    init main

---
+++ main.py
@@ -0,0 +1,14 @@
+# import strformat
+
+#Índice é o endereço virtual, e ao acessar o endereço virtual, conseguimos o índice para acessar a memoria física
+#Variável com espaço de endereços virtuais
+tabela = []
+
+#Variável com endereços fisicos
+memoria = []
+
+def main():
+    print("aaaaaaa")
+
+if __name__ == "__main__":
+    main()
