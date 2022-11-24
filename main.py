1cca7f1145ca39267a9fdc55359c62f08f238ea0
MariaPinguelli <mariaferpinguelli@hotmail.com>
2022-11-24 17:17:08 -0300

    imagem de apoio e estrutura de tabela de pagina

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 # import strformat
+#Variáveis globais
+memoriaFisicaPag = 0
 
 #Índice é o endereço virtual, e ao acessar o endereço virtual, conseguimos o índice para acessar a memoria física
 #Variável com espaço de endereços virtuais
-tabela = []
+memoriaVirtual = []
 
 #Variável com endereços fisicos
-memoria = []
+memoriaFisica = []
 
-tabelaHash = []
+tabelaPaginas = []
 
 def substituirPaginaAleatorio():
-    print("Substituir página aleatório")
+    print("Substituir pagina aleatorio")
     
 def substituirPaginaSC():
-    print("Substituir página SC")
+    print("Substituir pagina SC")
 
-def hashEndereco():
-    print("funcao hash")
+def hashEndereco(indice):
+    enderecoHash = indice%memoriaFisicaPag
+    return enderecoHash
+
+def processo(qtdPag, nomeProcesso):
+    global memoriaFisicaPag
+    
+    memoriaFisicaPag += qtdPag/4
+    
+    print("Nome do Processo: " + nomeProcesso)
+    print("Quantidade de paginas do processo: " + str(qtdPag))
+    print("Quantidade de paginas da memoria fisica: " + str(memoriaFisicaPag))
+    
+    
 
 def main():
-    print("main")
+    processo(12, "Processo aleatorio A")
+    print("\n")
+    processo(20, "Processo aleatorio B")
+    
 
 if __name__ == "__main__":
     main()
