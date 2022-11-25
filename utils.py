1ca00223619062abca04bfdb0c5fd4c95ca2381f
MariaPinguelli <mariaferpinguelli@hotmail.com>
2022-11-25 16:12:18 -0300

    lista nula

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
 def hashEndereco(indice, memoriaFisicaPag):
     enderecoHash = indice%memoriaFisicaPag
     return enderecoHash
+
+def criaListaNula(tam):
+    listaNula = []
+    for i in range(tam):
+        listaNula.append(None)
+    return listaNula
