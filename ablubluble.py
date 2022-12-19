6c6baa3ae3272fa02cdffc5ed7b91e96ff6512f9
Eduarda <eduarda_smartins@outlook.com>
2022-12-19 01:19:28 -0300

    Dias de luta

---
+++ ablubluble.py
@@ -0,0 +1,23 @@
+
+    # index = 0
+    # tam = int(abs(contPag(listaProc)/4))
+    # print('\n')
+    # for i in range(tam):
+    #     print("Index lista de acessos: "+str(index) +
+    #           " - Id da pagina: " + str(listaDeAcesso[i].id) +
+    #           " - Posição memória física: "+str(i))
+    #     listaDeAcesso[i].updateAloc(True, i)
+    #     memoriaFisica[i] = listaDeAcesso[i]
+    #     index += 1
+
+    # while (index < len(listaDeAcesso)):
+    #     pos = randint(0, len(memoriaFisica))
+    #     if (listaDeAcesso[index].isAloc == False):
+    #         listaDeAcesso[pos].updateAloc(True, listaDeAcesso[pos])
+    #         memoriaVirtual[pos] = listaDeAcesso[index]
+
+    #     print("Index lista de acessos: "+str(index) +
+    #           " - Id da pagina: " + str(listaDeAcesso[index].id)+
+    #           " - Posição memória física: "+str(pos))
+
+    #     index += 1
